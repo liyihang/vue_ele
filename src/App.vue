ef24d9a0bce24fd72a480069e177693070738b44
liyihang <lidoudoou@gmail.com>
2018-11-26 15:38:20 +0800

    router pages

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,34 @@
-<template>
+ <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
+
+    </div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import header from './components/header/header'
+import goods from './components/goods/goods'
+import ratings from './components/ratings/ratings'
+import seller from './components/seller/seller'
 export default {
-  name: 'App'
+  components: {
+    'v-header': header,
+    goods,
+    ratings,
+    seller
+  }
 }
 </script>
 
@@ -19,5 +40,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#app .tab {
+  display: flex;
+  widows: 100%;
+  height: 40px;
+  line-height: 40px;
+}
+.tab .tab-item {
+  flex: 1;
+  text-align: center;
 }
 </style>
